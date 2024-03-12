d51b6954af761ca89af6f59ad5e1305958e777d9
Matthieu Grellier <matthieu.grellier@gmail.com>
2024-03-12 14:32:13 +0100

    fix script

--- kubernetes/apps/dev/magicmirror/app/scripts/install-modules.sh
+++ kubernetes/apps/dev/magicmirror/app/scripts/install-modules.sh
@@ -1,12 +1,15 @@
 echo initializing mm modules
 cd /opt/magic_mirror/modules
 set -ex
-cat /tmp/config/modules.ini | while read module
+#delete all directories except the default one
+find ./ -mindepth 1 -name default -prune -o -exec rm -rf {} +
+MODULES="/tmp/config/modules.ini"
+while read -r module || [ -n "$module" ]
 do
-  echo installing $module
+  echo "installing $module"
   git clone $module
   folder=$(echo $module | sed -r 's|.*\/(.*)|\1|g' | xargs)
   cd $folder
   npm install --omit=dev || true
   cd ..
-done
+done < $MODULES
